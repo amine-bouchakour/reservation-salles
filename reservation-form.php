84fffc4b3a0509b53a7f334ebe7228d5674bf095
Bouchakour <amine.bouchakour@laplateforme.io>
2020-01-15 21:23:33 +0100

    Mise en place de toutes les contraintes pour la réservation

--- reservation-form.php
+++ reservation-form.php
@@ -50,27 +50,21 @@ if(isset($_SESSION['login']) and isset($_SESSION['ID']))
 
 
 
-function reservations ()
+function reservationsform ()
 
 {
+    $connexion= mysqli_connect("localhost","root","","reservationsalles");
+    $requete = "SELECT id FROM utilisateurs WHERE login='".$_SESSION['login']."' ";
+    $query = mysqli_query($connexion,$requete);
+    $resultat = mysqli_fetch_row($query);
+    $_SESSION['ID']=$resultat[0];
 
-
-
-$connexion= mysqli_connect("localhost","root","","reservationsalles");
-$requete = "SELECT id FROM utilisateurs WHERE login='".$_SESSION['login']."' ";
-$query = mysqli_query($connexion,$requete);
-$resultat = mysqli_fetch_row($query);
-$_SESSION['ID']=$resultat[0];
-
-    
-if(isset($_POST['valider']))
-    
-    {
-        echo 'étape 1 Bon'.'<br/>';
-
+    if(isset($_POST['valider']))
+        
+        {
         $date = date("d/m/Y : H:i:s");
         $datexdeb = date("d/m/Y : 08:00:00");
-        $datexfin = date("d/m/Y : 19:00:00");
+        $datexfin = date("d/m/Y : 22:00:00");
 
         // VERIFICATION SI JOUR DE SEMAINE POUR RESERVE LUNDI A VENDREDI
         if (date('l')=='Saturday' or date('l')=='Sunday')
@@ -87,7 +81,7 @@ if(isset($_POST['valider']))
                     $h1 = strtotime($_POST['debut']);
                     $h2 = strtotime($_POST['fin']);
                     $Start = gmdate("H", $h2-$h1);
-
+                    
                     if(!empty($_POST['titre']) and !empty($_POST['description']) and !empty($_POST['debut']) and !empty($_POST['fin']))
                     {
 
@@ -96,66 +90,92 @@ if(isset($_POST['valider']))
                         $dated=$_POST['debut'][8].$_POST['debut'][9];
                         $dateh=$_POST['debut'][11].$_POST['debut'][12];
                         $datedh=$_POST['debut'][8].$_POST['debut'][9].$_POST['debut'][11].$_POST['debut'][12];
-
+                        $datehp=$_POST['debut'][14].$_POST['debut'][15];
+                        $datedebut=$_POST['debut'][8].$_POST['debut'][9];
+                        $datefin=$_POST['fin'][8].$_POST['fin'][9];
 
                         // VERIFICATION SI ANNEE EN COURS
                         if(($date = date("Y"))==$datey)
                             {
-                                echo 'good'.'<br/>';
                                 
                                 // VERIFICATION SI MOIS EN COURS
                                 if(($date = date("m"))==$datem)
                                 {
-                                    echo 'good 11'.'<br/>';
 
                                     if(($date = date("d"))<=$dated)
                                     {
                                         if(($date = date("dH"))<$datedh )
                                         {
                                           
-                                            if($dateh>=8 and $dateh<=18)
+                                            if($dateh>=8 and $dateh<=22)
                                             {
                                                 
+                                                if($datehp==0)
+                                                {
                                                 
+                                                    if($datedebut==$datefin)
+                                                    {
+                                                    
+                                                        if($Start==1)
+                                                        {
+                                                            $connexion= mysqli_connect("localhost","root","","reservationsalles");
+                                                            $requete1 = "SELECT debut FROM reservations WHERE debut='".$_POST['debut']."'";
+                                                            $query1 = mysqli_query($connexion,$requete1);
+                                                            $resultat1 = mysqli_fetch_row($query1);
 
-                                                if($Start==1)
-                                                {
-                                                    echo 'GOOD DAY AND HOUR AGAIN §§§§§'.'<br/>';  
-                                                    $requete = "INSERT INTO `reservations` (`id`, `titre`, `description`, `debut`, `fin`, `id_utilisateur`) VALUES (NULL, '".$_POST['titre']."', '".$_POST['description']."', '".$_POST['debut']."', '".$_POST['fin']."', '".$_SESSION['ID']."');";
-                                                    $query= mysqli_query($connexion, $requete);
-                                                    echo 'étape 2 Bon'.'<br/>';
-                                                }
-                                                else if($Start==0)
-                                                {
-                                                    echo 'Vous n\'avez fait aucune réservation'.'<br/>';
+                                                            if($resultat1==0)
+                                                            {
+                                                            echo 'GOOD DAY AND HOUR AGAIN §§§§§'.'<br/>';  
+                                                            $requete2 = "INSERT INTO `reservations` (`id`, `titre`, `description`, `debut`, `fin`, `id_utilisateur`) VALUES (NULL, '".$_POST['titre']."', '".$_POST['description']."', '".$_POST['debut']."', '".$_POST['fin']."', '".$_SESSION['ID']."');";
+                                                            $query2= mysqli_query($connexion, $requete2);
+                                                            }
+
+                                                            else 
+                                                            {
+                                                                echo 'La salle est déjà réservé pour cette heure-ci'.'<br/>';
+                                                            }
+                                                        }
+
+                                                        else if($Start==0)
+                                                        {
+                                                            echo 'Vous n\'avez fait aucune réservation'.'<br/>';
+                                                        }
+                                                    }
+
+                                                    else
+                                                    {
+                                                        echo 'Vos dates de début d\'évenement et de fin doivent être le même jour'.'<br/>';
+                                                    }
                                                 }
 
+                                                else
+                                                {
+                                                    echo "Les réservations se font sur des heures pleines, ex: 9:00, 14:00.".'<br/>';
+                                                }
                                             }
 
                                             else
                                             {
                                                 echo "Les salles sont accessible que de 8h à 19h".'<br/>';
                                             }
-
                                         }
 
                                         else
                                         {
                                         echo "Vous ne pouvez pas réservé pour l'heure en cours.".'<br/>';
                                         }
-
                                     }
+
                                     else 
                                     {
                                         echo "Vous ne pouvez pas réservé à une date antérieure à celle d'aujourd'hui.".'<br/>';
                                     }
-
                                 }
+
                                 else 
                                 {
                                     echo "Vous ne pouvez pas réservé à une date antérieure à celle d'aujourd'hui.".'<br/>';
                                 }
-                        
                             }
 
                             else 
@@ -176,22 +196,32 @@ if(isset($_POST['valider']))
     
                 }
             }
-
-        
+        }
     }
 
 }
 
-reservations();
+reservationsform();
 
-}
-$date = date("Y/m/d H:i");
-$datedh=$_POST['debut'][8].$_POST['debut'][9].$_POST['debut'][11].$_POST['debut'][12];
+
+// $date = date("Y/m/d H:i");
+// $datedh=$_POST['debut'][8].$_POST['debut'][9].$_POST['debut'][11].$_POST['debut'][12];
                         
 
-$date = date("dH");
-echo $date.'<br/>';
-echo $datedh;
+// $date = date("dH");
+// echo $date.'<br/>';
+// echo $datedh.'<br/>';
+
+
+// $date = date("i");
+// echo $date.'<br/>';
+// $datehp=$_POST['debut'][14].$_POST['debut'][15];
+
+
+
+
+
+
 
     
 
