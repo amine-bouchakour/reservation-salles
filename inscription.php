a69ed2efcc4349e5d61b4f5ade2fc90bf7c123b0
Bouchakour <amine.bouchakour@laplateforme.io>
2020-01-18 23:47:24 +0100

    suppression texte indication

--- inscription.php
+++ inscription.php
@@ -1,8 +1,3 @@
-Le formulaire doit contenir l’ensemble des champs présents dans la table
-“utilisateurs” (sauf “id”) ainsi qu’une confirmation de mot de passe. Dès
-qu’un utilisateur remplit ce formulaire, les données sont insérées dans la
-base de données et l’utilisateur est redirigé vers la page de connexion.
-
 <html>
 <title>inscription</title>
 
