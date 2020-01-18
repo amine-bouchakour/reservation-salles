d2edd3d71c053fe940b2c0bb5ef6555c3783371c
Bouchakour <amine.bouchakour@laplateforme.io>
2020-01-18 23:46:08 +0100

    suppression texte indication

--- connexion.php
+++ connexion.php
@@ -1,8 +1,3 @@
-Le formulaire doit avoir deux inputs : “login” et “password”. Lorsque le
-formulaire est validé, s’il existe un utilisateur en bdd correspondant à ces
-informations, alors l’utilisateur devient connecté et une (ou plusieurs)
-variables de session sont créées.
-
 <html>
 <title>Connexion</title>
 
