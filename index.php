2bf1567aaa8d4f317549475c01aa3bf92f356feb
Bouchakour <amine.bouchakour@laplateforme.io>
2020-01-19 20:45:25 +0100

    ajout fonction ucfirst pour l'affichage du login de session

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <html>
+    <head>
 <title>Index</title>
-
+    </head>
 
 <?php
 session_start();
@@ -14,6 +15,7 @@ if(isset($_SESSION['login']))
     <a href="reservation-form.php">Formulaire de réservations</a> <br>
     <a href="deconnexion.php">Se déconnecter</a> <br>
     <?php
+    $_SESSION['login']=ucfirst($_SESSION['login']);
     echo '<br/>'.'Bienvenue à toi '.$_SESSION['login'].'<br/>'.'<br/>';
 }
 else 
