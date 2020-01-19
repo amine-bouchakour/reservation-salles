d97ae3f4744e5415f6d7698523b83cfcba300d46
Bouchakour <amine.bouchakour@laplateforme.io>
2020-01-20 00:59:36 +0100

    suppresion fonction affichage header

--- index.php
+++ index.php
@@ -8,27 +8,11 @@
 <?php
 session_start();
 
+include('fonctions.php');
+headmenu();
 
-if(isset($_SESSION['login']))
-{
-    ?>
-    <a href="profil.php">Profil</a><br>
-    <a href="planning.php">Planning</a> <br>
-    <a href="reservation-form.php">Formulaire de réservations</a> <br>
-    <a href="deconnexion.php">Se déconnecter</a> <br>
-    <?php
-    $_SESSION['login']=ucfirst($_SESSION['login']);
-    echo '<br/>'.'Bienvenue à toi '.$_SESSION['login'].'<br/>'.'<br/>';
-}
-else 
-{
-   ?>
-    <a href="planning.php">Planning</a> <br>
-    <a href="connexion.php">Se connecter</a><br>
-    <a href="inscription.php">S'inscrire</a><br>
-    <?php
-    echo '<br/>'.'Bienvenue à toi '.'<br/>'.'<br/>';
-}
+
+
 
 
 
