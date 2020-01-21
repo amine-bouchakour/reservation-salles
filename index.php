596f4f22d28c6092a15a6484b56676de28fad1de
Bouchakour <amine.bouchakour@laplateforme.io>
2020-01-21 12:04:15 +0100

    ajout texte sur page index

--- index.php
+++ index.php
@@ -15,15 +15,22 @@ session_start();
 include('fonctions.php');
 headmenu();
 
-if(isset($_SESSION['login']))
-{
-    echo 'Bienvenue à toi '.$_SESSION['login'].'<br/>';
-}
-
-
 
 
 ?>
+
+
+<div class="copy2">
+    <p>Bienvenue sur notre site<?php if(isset($_SESSION['login'])){echo ' '.$_SESSION['login'];}?>.</p>
+    <p>Nous vous proposons la réservation de salle pour divers activités.</p>
+    <p>La location est gratuite.</p>
+    <p>Vous pouvez réservé une salle de 8h à 19h du lundi au vendredi et ceci pour une durée d'une heure maximum.</p>
+    <p>Le planning affiche toutes les réservations déjà faites et les différentes disponibilités.</p>
+    <p>N'hésitez à aller jeter un coup d'oeil <a href="planning.php"> en cliquant ici.</a></p>
+    <p></p>
+
+</div>
+
 
 <footer>
     <div class="copy">
