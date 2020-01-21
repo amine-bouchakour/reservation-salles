9f9ca652c188f77e2d7bc33720216b131786260d
Bouchakour <amine.bouchakour@laplateforme.io>
2020-01-21 10:37:22 +0100

    ajout footer

--- index.php
+++ index.php
@@ -5,18 +5,31 @@
 
     </head>
 
+    <body>
+        
+    
+
 <?php
 session_start();
 
 include('fonctions.php');
 headmenu();
 
-
+if(isset($_SESSION['login']))
+{
+    echo 'Bienvenue à toi '.$_SESSION['login'].'<br/>';
+}
 
 
 
 
 ?>
 
+<footer>
+    <div class="copy">
+    © 2019-2029 Bouchakour Amine All right reserved.
+    </div>
+</footer>
 
+</body>
 </html>
