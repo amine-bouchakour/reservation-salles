524416611f01e3b470a2a7d3eb10799751ebea4c
Bouchakour <amine.bouchakour@laplateforme.io>
2020-01-22 01:05:49 +0100

    corrections lignes

--- index.php
+++ index.php
@@ -21,12 +21,12 @@ headmenu();
 
 
 <div class="copy2">
-    <p>Bienvenue sur notre site<?php if(isset($_SESSION['login'])){echo ' '.$_SESSION['login'];}?>.</p>
-    <p>Nous vous proposons la réservation de salle pour divers activités.</p>
-    <p>La location est gratuite.</p>
-    <p>Vous pouvez réservé une salle de 8h à 19h du lundi au vendredi et ceci pour une durée d'une heure maximum.</p>
-    <p>Le planning affiche toutes les réservations déjà faites et les différentes disponibilités.</p>
-    <p>N'hésitez à aller jeter un coup d'oeil <a href="planning.php"> en cliquant ici.</a></p>
+    <p>Bienvenue sur notre site <div class="login"><?php if(isset($_SESSION['login'])) {echo ' '.$_SESSION['login'].' :)';}?></div></p>
+    <p>Ici nous vous proposons la réservation de salle pour divers activités</p>
+    <p>La location est gratuite !</p>
+    <p>Vous pouvez réservé une salle de 8h à 19h du lundi au vendredi et ceci pour une durée d'une heure maximum</p>
+    <p>Le planning affiche toutes les réservations déjà faites et les différentes disponibilités</p>
+    <p>N'hésitez à aller jeter un coup d'oeil <a href="planning.php"> en cliquant ici</a></p>
     <p></p>
 
 </div>
