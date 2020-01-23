6c5dce5fb40eeb8e7c289484b81bab02085ee104
Bouchakour <amine.bouchakour@laplateforme.io>
2020-01-23 17:28:55 +0100

    sql

--- reservationsalles.sql
+++ reservationsalles.sql
@@ -1,2 +1,80 @@
 Vous devez également rendre la structure et le contenu de votre base de
 données dans une fichier nommé “reservationsalles.sql”.
+
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  jeu. 23 jan. 2020 à 16:27
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `reservationsalles`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reservations`
+--
+
+DROP TABLE IF EXISTS `reservations`;
+CREATE TABLE IF NOT EXISTS `reservations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `titre` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `debut` datetime NOT NULL,
+  `fin` datetime NOT NULL,
+  `id_utilisateur` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=170 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `reservations`
+--
+
+INSERT INTO `reservations` (`id`, `titre`, `description`, `debut`, `fin`, `id_utilisateur`) VALUES
+(162, 'musique', 'rÃ©pÃ©tition', '2020-01-22 13:00:00', '2020-01-22 14:00:00', 33),
+(163, 'Dessin', 'Recherche de croquis pour illustration', '2020-01-24 10:00:00', '2020-01-24 11:00:00', 31),
+(164, 'thÃ©Ã¢tre', 'rÃ©pÃ©tition mise en scÃ¨ne', '2020-01-27 11:00:00', '2020-01-27 12:00:00', 34);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateurs`
+--
+
+DROP TABLE IF EXISTS `utilisateurs`;
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=42 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `login`, `password`) VALUES
+(33, 'jacques', '$2y$10$LhuHi2XeU68D6JlQ1awJ0uvUm2ES8Xx2T6hJf0nMJ9TsZiG57IE3a'),
+(31, 'paul', '$2y$10$XYn4UruqCqWQ0IFmyURl/eRNuL8pyl9uAW6mIvyNcAULm2vsH9vFG'),
+(34, 'aurÃ©lie', '$2y$10$Qh8HW881SFBdJ6PKD/XNl.x6nEzulvFm3BqFqzJjvxh5/3vPCg4Se');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
