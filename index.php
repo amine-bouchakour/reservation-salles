4883fb8f0f2fdaa9aff292c2b667c775b3c95a36
Bouchakour <amine.bouchakour@laplateforme.io>
2020-02-10 10:02:32 +0100

    modification html pour bon affichage footer

--- index.php
+++ index.php
@@ -1,42 +1,54 @@
 <html>
-    <head>
-<title>Index</title>
-<link rel="stylesheet" href="planning.css">
 
-    </head>
+<head>
+    <title>Index</title>
+    <link rel="stylesheet" href="planning.css">
 
-    <body>
-        
-    
+</head>
 
-<?php
+<body>
+
+
+
+    <?php
 session_start();
 
 include('fonctions.php');
-headmenu();
 
 
 
 ?>
+    <div id="page">
+        <div id="header"><?php headmenu(); ?></div>
+        <div id="content">
+            <div class="copy2">
+                <p>Bienvenue sur notre site <div class="login">
+                        <?php if(isset($_SESSION['login'])) {echo ' '.$_SESSION['login'].' :)';}?></div>
+                </p>
+                <p>Ici nous vous proposons la réservation de salle pour divers activités</p>
+                <p>La location est gratuite !</p>
+                <p>Vous pouvez réservé une salle de 8h à 19h du lundi au vendredi et ceci pour une durée d'une heure
+                    maximum</p>
+                <p>Le planning affiche toutes les réservations déjà faites et les différentes disponibilités</p>
+                <p>N'hésitez à aller jeter un coup d'oeil <a href="planning.php"> en cliquant ici</a></p>
+                <p></p>
 
-
-<div class="copy2">
-    <p>Bienvenue sur notre site <div class="login"><?php if(isset($_SESSION['login'])) {echo ' '.$_SESSION['login'].' :)';}?></div></p>
-    <p>Ici nous vous proposons la réservation de salle pour divers activités</p>
-    <p>La location est gratuite !</p>
-    <p>Vous pouvez réservé une salle de 8h à 19h du lundi au vendredi et ceci pour une durée d'une heure maximum</p>
-    <p>Le planning affiche toutes les réservations déjà faites et les différentes disponibilités</p>
-    <p>N'hésitez à aller jeter un coup d'oeil <a href="planning.php"> en cliquant ici</a></p>
-    <p></p>
-
-</div>
-
-
-<footer>
-    <div class="copy">
-    © 2019-2029 Bouchakour Amine All right reserved.
+            </div>
+        </div>
+        <div id="footer">
+            <div>
+                <div class="copy">
+                    © 2019-2029 Bouchakour Amine All right reserved.
+                </div>
+            </div>
+        </div>
     </div>
-</footer>
+
+
+
+
+
 
 </body>
+
 </html>
